53befc087eb58469a972563b61282ba91cdb2110
yunfwe <1441923087@qq.com>
2017-08-02 15:49:56 +0800

    Hostscan

--- src/components/Hostscan.vue
+++ src/components/Hostscan.vue
@@ -4,56 +4,125 @@
     <form @submit.prevent="onSubmit()"  class="layui-form">
         <div class="layui-inline">
           <div class="layui-input-inline">
-            <input type="text" name="iprange" placeholder="输入单个IP或者网段"
-                   class="layui-input">
+            <input v-model="iprange" type="text" name="iprange" placeholder="输入单个IP或者网段"
+                   class="layui-input" required>
           </div>
           <div class="layui-input-inline">
-            <button type="submit" class="layui-btn">扫描</button>
+            <button type="submit" class="layui-btn layui-btn-primary" :class="{'layui-disabled': showLoad}" >
+              <i class="layui-icon" style="display: inline-block;font-size: 25px; color: #0f0f0f;">&#xe615;</i>
+            </button>
+            <!--<button type="submit" class="layui-btn">扫描</button>-->
+          </div>
+          <div class="layui-input-inline" style="margin-left: 30px" v-if="showLoad">
+            <i class="layui-icon layui-anim layui-anim-rotate layui-anim-loop"
+               style="display: inline-block;font-size: 25px; color: #0f0f0f;">&#xe63d;
+            </i>
           </div>
         </div>
     </form>
-    <div v-for="item in iprst" class="result">
-      IP: {{item.ip}}  result:{{item.result}}
+    <div class="result">
+      <table class="layui-table">
+        <colgroup>
+          <col width="70">
+          <col width="200">
+          <col width="150">
+          <col>
+        </colgroup>
+        <thead>
+        <tr>
+          <th>编号</th>
+          <th>IP</th>
+          <th>状态</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(item,index) in iprst">
+          <td>{{index+1}}</td>
+          <td>{{item.ip}}</td>
+          <td v-if="item.result" style="color: green">online</td>
+          <td v-else style="color: red">offline</td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 <script>
+  import urls from '../config/urls'
   export default {
     name:'vhostscan',
     data: ()=>{
         return {
-            iprst:[
-              {ip:'192.168.1.1', result:true},
-              {ip:'192.168.1.3', result:false},
-              {ip:'192.168.1.4', result:true},
-              {ip:'192.168.1.5', result:false},
-              {ip:'192.168.1.1', result:true},
-              {ip:'192.168.1.3', result:false},
-              {ip:'192.168.1.4', result:true},
-              {ip:'192.168.1.5', result:false},
-              {ip:'192.168.1.1', result:true},
-              {ip:'192.168.1.3', result:false},
-              {ip:'192.168.1.4', result:true},
-              {ip:'192.168.1.5', result:false},
-              {ip:'192.168.1.1', result:true},
-              {ip:'192.168.1.3', result:false},
-              {ip:'192.168.1.4', result:true},
-              {ip:'192.168.1.5', result:false},
-              {ip:'192.168.1.1', result:true},
-              {ip:'192.168.1.3', result:false},
-              {ip:'192.168.1.4', result:true},
-              {ip:'192.168.1.5', result:false},
-              {ip:'192.168.1.1', result:true},
-              {ip:'192.168.1.3', result:false},
-              {ip:'192.168.1.4', result:true},
-              {ip:'192.168.1.5', result:false},
+          iprange:'',
+          showLoad:false,
+          iprst:[
+//              {ip:'192.168.1.1', result:true},
+//              {ip:'192.168.1.3', result:false},
+//              {ip:'192.168.1.4', result:true},
+//              {ip:'192.168.1.5', result:false},
+//              {ip:'192.168.1.1', result:true},
+//              {ip:'192.168.1.3', result:false},
+//              {ip:'192.168.1.4', result:true},
+//              {ip:'192.168.1.5', result:false},
+//              {ip:'192.168.1.1', result:true},
+//              {ip:'192.168.1.3', result:false},
+//              {ip:'192.168.1.4', result:true},
+//              {ip:'192.168.1.5', result:false},
+//              {ip:'192.168.1.1', result:true},
+//              {ip:'192.168.1.3', result:false},
+//              {ip:'192.168.1.4', result:true},
+//              {ip:'192.168.1.5', result:false},
+//              {ip:'192.168.1.1', result:true},
+//              {ip:'192.168.1.3', result:false},
+//              {ip:'192.168.1.4', result:true},
+//              {ip:'192.168.1.5', result:false},
+//              {ip:'192.168.1.1', result:true},
+//              {ip:'192.168.1.3', result:false},
+//              {ip:'192.168.1.4', result:true},
+//              {ip:'192.168.1.5', result:false},
             ]
         }
     },
     methods:{
-      onSubmit:function () {
+      onSubmit: async function () {
+        if(this.showLoad){
+          layui.layer.msg('正在处理 请稍后...',{time:2000});
+          return
+        }
+        layui.layer.msg('请稍后...',{time:1000});
+        this.showLoad = true;
+        try{
+          console.log(urls.WS_PING);
+          let ws = new WebSocket(urls.WS_PING);
+          ws.onopen = ()=> {
+            ws.send(this.iprange);
+            this.iprst = []
+          };
+          ws.onmessage = (data)=>{
+              console.log(data.data);
+              this.iprst.push(JSON.parse(data.data))
+          };
+          ws.onerror = ()=>{
+            this.showLoad = false;
+            layui.layer.msg('网络连接失败',{time:1000})
+          };
+          ws.onclose = ()=>{
+            this.showLoad = false;
+            layui.layer.msg('扫描成功',{time:1000})
+          }
+        } catch(e) {
+          this.showLoad = false;
+          layui.layer.msg('网络连接失败',{time:1000})
+        }
 
       }
+    },
+    mounted:function () {
+//      setInterval( ()=> {
+//        this.iprst.push({ip:'192.168.1.1',result:true});
+//        this.iprst.push({ip:'192.168.1.2',result:false})
+//      },100)
+
     }
   }
 </script>
@@ -72,5 +141,6 @@
 
   .result{
     margin-top: 20px;
+    width: 400px;
   }
 </style>
--- src/config/urls.js
+++ src/config/urls.js
@@ -1,4 +1,5 @@
 var BASE_URL = 'http://192.168.8.253:8080/';
+var WS_BASE_URL = 'ws://192.168.8.253:8080/';
 
 export default {
   MONITOR_MEMINFO: BASE_URL+'meminfo',
@@ -6,4 +7,5 @@ export default {
   PXE_START_SERVE: BASE_URL+'pxe/start/',
   PXE_STOP_SERVE: BASE_URL+'pxe/stop/',
   PXE_RESTART_SERVE: BASE_URL+'pxe/restart/',
+  WS_PING: WS_BASE_URL+'ws/ping'
 }
